560c9710a0ae67e6df70763c92ed95aabe85d02f
Slaventij Bilokur <svsq@outlook.com>
2018-02-10 18:53:33 +0200

    Added asynctask class that connecting to rss url and fetching feed.

--- app/src/main/java/tk/slaventij/simplerss/MainActivity.java
+++ app/src/main/java/tk/slaventij/simplerss/MainActivity.java
@@ -1,15 +1,24 @@
 package tk.slaventij.simplerss;
 
+import android.os.AsyncTask;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
     private String feedTitle;
     private String feedLink;
     private String feedDescription;
+
+    public static final String TAG = "TAG";
 
 
     @Override
@@ -59,7 +70,58 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void fetch() {
-        new FetchFeedTask().execute((Void) null);
+    private void fetch() { new FetchFeedTask().execute((Void) null); }
+
+    private class FetchFeedTask extends AsyncTask<Void, Void, Boolean> {
+
+        private String urlLink;
+
+    /*public void execute(Void aVoid) {
+
+
+    }*/
+
+        @Override
+        protected void onPreExecute() {
+            swipeRefreshLayout.setRefreshing(true);
+            urlLink = editText.getText().toString();
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            swipeRefreshLayout.setRefreshing(false);
+
+            if(success) {
+                feedTitleTextView.setText("Feed Title: " + feedTitle);
+                feedDescriptionTextView.setText("Feed Description: " + feedDescription);
+                feedLinkTextView.setText("Feed Link: " + feedLink);
+                // fill recyclerview
+                recyclerView.setAdapter(new RSSFeedListAdapter(feedModelList));
+            } else {
+                Toast.makeText(MainActivity.this, "Enter a valid RSS URL!",
+                        Toast.LENGTH_LONG).show();
+            }
+            super.onPostExecute(success);
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            if(TextUtils.isEmpty(urlLink)) {
+                return false;
+            }
+            try {
+                if(!urlLink.startsWith("http://") && !urlLink.startsWith("https://")) {
+                    urlLink = "http://" + urlLink;
+                }
+
+                URL url = new URL(urlLink);
+                InputStream inputStream = url.openConnection().getInputStream();
+                feedModelList = parseFeed(inputStream);
+                return true;
+            } catch (IOException | XmlPullParserException e) {
+                Log.e(TAG, "Error", e);
+            }
+            return false;
+        }
     }
 }
